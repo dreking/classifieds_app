401ea5b106109ebb35da86e4b7bf44d8c6fcf38f
Leandre <leajapo@gmail.com>
2021-07-07 01:07:13 +0200

    impr: imprvoed product details screen

--- lib/screens/product_details_screen.dart
+++ lib/screens/product_details_screen.dart
@@ -1,9 +1,10 @@
 import 'package:flutter/material.dart';
-import 'package:share/share.dart';
 import 'package:intl/intl.dart';
 
+import 'package:classifieds_app/components/custom_button.dart';
 import 'package:classifieds_app/data/data.dart';
 import 'package:classifieds_app/models/product.dart';
+import 'package:share/share.dart';
 
 class ProductDetailsScreen extends StatelessWidget {
   static const routeName = '/product-details';
@@ -19,96 +20,107 @@ class ProductDetailsScreen extends StatelessWidget {
     final Product product =
         ModalRoute.of(context)!.settings.arguments as Product;
 
-    return Container(
-      decoration: BoxDecoration(
-        image: DecorationImage(
-          colorFilter:
-              ColorFilter.mode(Colors.grey.shade500, BlendMode.multiply),
-          image: NetworkImage(getImageUrl(product.imageUrl!)),
-          fit: BoxFit.cover,
-        ),
-      ),
-      child: Scaffold(
-        backgroundColor: Colors.transparent,
-        appBar: AppBar(
-          backgroundColor: Colors.transparent,
-          actions: [
-            IconButton(
-              icon: Icon(Icons.share),
-              onPressed: () {
-                Share.share(
-                  'check out this product https://https://classifiedsserver.herokuapp.com/api/v1/public/products${product.id}',
-                );
-              },
-            )
-          ],
-        ),
-        body: Padding(
-          padding: const EdgeInsets.all(20),
-          child: Column(
-            children: [
-              Spacer(),
-              SizedBox(height: 10),
-              TextDisplay(
-                title: 'Name: ',
-                text: product.name!,
-              ),
-              SizedBox(height: 10),
-              TextDisplay(
-                title: 'Category: ',
-                text: product.category!,
-              ),
-              SizedBox(height: 10),
-              TextDisplay(
-                title: 'Description: ',
-                text: product.description!,
-              ),
-              SizedBox(height: 10),
-              TextDisplay(
-                title: 'Manufactored: ',
-                text: format.format(product.manufactureDate!),
-              ),
-              SizedBox(height: 30),
-              TextDisplay(
-                title: 'Price: RWF',
-                text: product.price!.toStringAsFixed(2),
-              ),
-            ],
+    return Scaffold(
+      body: Column(
+        crossAxisAlignment: CrossAxisAlignment.stretch,
+        children: [
+          Container(
+            height: 350,
+            child: Stack(
+              children: [
+                Hero(
+                  tag: product.id!,
+                  child: ClipRRect(
+                    borderRadius: BorderRadius.only(
+                      bottomLeft: Radius.circular(50),
+                      bottomRight: Radius.circular(50),
+                    ),
+                    child: SafeArea(
+                      child: FadeInImage.assetNetwork(
+                        placeholder: 'assets/images/placeholder.png',
+                        image: getImageUrl(product.imageUrl!),
+                        height: 350,
+                        width: double.infinity,
+                        fit: BoxFit.cover,
+                      ),
+                    ),
+                  ),
+                ),
+                Positioned(
+                  top: 0,
+                  child: SafeArea(
+                    child: IconButton(
+                      onPressed: () => Navigator.of(context).pop(),
+                      icon: Icon(
+                        Icons.chevron_left,
+                        size: 40,
+                      ),
+                    ),
+                  ),
+                ),
+                Positioned(
+                  right: 10,
+                  child: SafeArea(
+                    child: IconButton(
+                      onPressed: () => Share.share(
+                        'check out this product https://https://classifiedsserver.herokuapp.com/api/v1/public/products${product.id}',
+                      ),
+                      icon: Icon(Icons.share, size: 30),
+                    ),
+                  ),
+                ),
+              ],
+            ),
           ),
-        ),
+          Expanded(
+            child: ListView(
+              padding: EdgeInsets.all(20),
+              children: [
+                Text(
+                  product.name!,
+                  style: Theme.of(context).textTheme.headline4,
+                  textAlign: TextAlign.start,
+                ),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Text(
+                      product.category!,
+                      style: Theme.of(context).textTheme.headline6,
+                      textAlign: TextAlign.start,
+                    ),
+                    Text(
+                      format.format(product.manufactureDate!),
+                      style: Theme.of(context).textTheme.headline6,
+                      textAlign: TextAlign.start,
+                    ),
+                  ],
+                ),
+                SizedBox(height: 20),
+                Text(
+                  'RWF ' + product.price!.toStringAsFixed(2),
+                  style: Theme.of(context).textTheme.headline5,
+                  textAlign: TextAlign.start,
+                ),
+                SizedBox(height: 20),
+                Text(
+                  product.description!,
+                  style: Theme.of(context).textTheme.headline6,
+                  textAlign: TextAlign.start,
+                ),
+              ],
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.all(10),
+            child: CustomButton(
+              onTap: () {},
+              text: 'Add to Cart',
+              icon: Icons.shopping_cart_outlined,
+            ),
+          )
+        ],
       ),
-      // body: CustomScrollView(
-      //   slivers: [
-      //     SliverAppBar(
-      //       expandedHeight: 250,
-      //       pinned: true,
-      //       flexibleSpace: FlexibleSpaceBar(
-      //         title: Text(product.name!),
-      //         background: Hero(
-      //           tag: product.id!,
-      //           child: FadeInImage.assetNetwork(
-      //             placeholder: 'assets/images/placeholder.png',
-      //             image: getImageUrl(product.imageUrl!),
-      //             height: 300,
-      //             width: double.infinity,
-      //             fit: BoxFit.cover,
-      //           ),
-      //         ),
-      //       ),
-      //     ),
-      //     SliverList(
-      //       delegate: SliverChildListDelegate(
-      //         [
-      //           SizedBox(height: 20),
-      //           TextDisplay(
-      //             title: 'Description: ',
-      //             text: product.description!,
-      //           ),
-      //         ],
-      //       ),
-      //     ),
-      //   ],
-      // ),
     );
   }
 }
@@ -128,12 +140,12 @@ class TextDisplay extends StatelessWidget {
       children: [
         Text(
           title,
-          style: Theme.of(context).textTheme.bodyText1,
+          style: Theme.of(context).textTheme.bodyText2,
         ),
         Flexible(
           child: Text(
             text,
-            style: Theme.of(context).textTheme.bodyText1,
+            style: Theme.of(context).textTheme.bodyText2,
           ),
         ),
       ],
